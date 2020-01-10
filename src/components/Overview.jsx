18ce3534f2af2767e19ce6d4df732760b7ee0d44
proforty7 <proforty7@gmail.com>
2020-01-10 18:56:37 +0530

    [feature-dashboard]add overview
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import moment from "moment";
 
 const Overview = () => {
   const currentProject = useSelector(state => state.project.currentProject);
 
-  return <div>{currentProject.projectName}</div>;
+  return (
+    <div>
+      <div>{currentProject.projectName}</div>
+      <div>{currentProject.description}</div>
+      {currentProject.productionVersion}
+      <div>
+        Started on
+        <br />
+        {moment(currentProject.startedOn).format("DD-MM-YYYY")}
+      </div>
+      <div>
+        Last Updated on
+        <br />
+        {moment(currentProject.lastUpdatedOn).format("DD-MM-YYYY")}
+      </div>
+    </div>
+  );
 };
 
 export default Overview;
