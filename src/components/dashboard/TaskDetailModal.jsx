6d3f44a67d0b56c6b6d963e558875e14df57d73e
proforty7 <proforty7@gmail.com>
2020-01-15 13:26:24 +0530

    add tasks
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- src/components/dashboard/TaskDetailModal.jsx
+++ src/components/dashboard/TaskDetailModal.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import { Modal } from "react-bootstrap";
 
 const TaskDetailModal = ({ task, handleHideModal, setRefresh }) => {
+  console.log(task);
+  const { title, description, assignedTo, assignedBy, priority } = task;
+
+  const renderAssignedToList = () => {
+    return assignedTo.map(contributor => <div>{contributor.profile.name}</div>);
+  };
+
   return (
     <>
-      <Modal.Header>{task.title}</Modal.Header>
+      <Modal.Header>{title}</Modal.Header>
+      <Modal.Body>
+        <div>{description}</div>
+        <div>
+          Assigned to: <br />
+          {renderAssignedToList()}
+        </div>
+        <div>Assigned by: {assignedBy.profile.name}</div>
+        <div>Priority: {priority}</div>
+      </Modal.Body>
     </>
   );
 };
